9b32f302061307bbcbbf037f52df1cbebf860731
jfechete <60266095+jfechete@users.noreply.github.com>
2022-12-20 18:20:31 -0800

    Made constructors for data classes

--- data_classes.py
+++ data_classes.py
@@ -1,12 +1,19 @@
 class Paths:
-    pass
+    def __init__(self, path_list):
+        self._path_list = path_list
 
 class Points:
-    pass
+    def __init__(self, point_list):
+        self._point_list = point_list
 
 class Path:
-    pass
+    def __init__(self, point_list):
+        self._point_list = point_list
 
 class Point:
-    pass
+    def __init__(self, x, y):
+        #Coordinates are currently meant to be pixels and integers
+        #May be changed in the future as non-image inputs are added
+        self.x = x
+        self.y = y
 
