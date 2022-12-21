384186da4f164d08a29b95a78f149f3e5d8c1143
jfechete <60266095+jfechete@users.noreply.github.com>
2022-12-20 20:05:04 -0800

    Added getting points from binary image outline

--- data_classes.py
+++ data_classes.py
@@ -8,6 +8,43 @@ class Points:
     def __init__(self, point_list):
         self._point_list = point_list
 
+    @staticmethod
+    def from_binary_image(pil_image):
+        """
+        Returns a Points object showing the outline of the white objects in this picture.
+        The inner border is used, every point will be on a white pixel.
+        """
+        if pil_image.mode != "1":
+            pil_image = pil_image.convert("1")
+
+        point_list = []
+        for x in range(pil_image.width):
+            for y in range(pil_image.height):
+                color = pil_image.getpixel((x,y))
+                if color > 0:
+                    neighs = (
+                        (x+1, y),
+                        (x-1, y),
+                        (x, y+1),
+                        (x, y-1)
+                    )
+                    for neigh in neighs:
+                        neigh_color = pil_image.getpixel(neigh)
+                        if neigh_color == 0:
+                            point_list.append(Point(x, y))
+                            break
+
+        return Points(point_list)
+
+    def visualize_points(self, background_image, color):
+        """"
+        Shows the background image but with every point added to it using the color parameter.
+        """
+        background_image = background_image.copy()
+        for point in self:
+            background_image.putpixel((point.x, point.y), color)
+        background_image.show()
+
     def __iter__(self):
         return iter(self._point_list)
 
@@ -24,6 +61,6 @@ class Point:
         #May be changed in the future as non-image inputs are added
         self.x = x
         self.y = y
-    
+
     def __str__(self):
         return "{}, {}".format(self.x, self.y)
--- nc_generator.py
+++ nc_generator.py
@@ -2,15 +2,13 @@ from PIL import Image
 import data_classes
 import generator
 
+INPUT_FILE = "in.png"
+POINTS_VISUALIZE_COLOR = (255, 0, 0)
+
 def main():
-    points = []
-    for i in range(10):
-        points.append(data_classes.Point(i,0))
-    
-    for p in data_classes.Points(points):
-        print(p)
-    for p in data_classes.Path(points):
-        print(p)
+    image = Image.open(INPUT_FILE)
+    points = data_classes.Points.from_binary_image(image)
+    points.visualize_points(image, POINTS_VISUALIZE_COLOR)
 
 if __name__ == "__main__":
     main()
